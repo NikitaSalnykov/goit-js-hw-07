2773296aab5aa21d696abc720e3c4ffa224fe54f
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-04-21 02:02:13 +0300

    ++

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -8,13 +8,43 @@ const photos = galleryItems.map((photo) =>
   `
   <li class="gallery__item" >
   <a class="gallery__link">
-  <img class="gallery__image" src=${photo.preview} alt='${photo.description}'>
+  <img class="gallery__image"
+  src=${photo.preview} 
+  data-source=${photo.original} 
+  alt='${photo.description}'>
   </a>
   </li>
   `
 ).join("")
  
 galleryList.insertAdjacentHTML("afterbegin", photos)
-console.log(galleryList);
 
-galleryList.
+galleryList.addEventListener('click', (event) => {
+
+  if (event.target.nodeName !== "IMG") {
+    return
+  }
+
+  photos = galleryItems.map((photo) => {
+    if (event.target.dataset.source !== photo.original) {
+    return
+  }
+  const instance = basicLightbox.create(`
+    <img src='${photo.original}' width="800" height="600">
+`)
+    instance.show()
+
+    
+  document.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+      instance.close()
+    }
+  })
+
+  })
+
+
+})
+
+
+  
