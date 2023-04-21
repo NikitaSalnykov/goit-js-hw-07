bb367c4248997396a8078d5882f618685a5b9786
NikitaSalnykov <salnikov.nkt@gmail.com>
2023-04-21 18:04:30 +0300

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,7 +7,7 @@ const galleryList = document.querySelector('.gallery')
 const photos = galleryItems.map((photo) => 
   `
   <li class="gallery__item" >
-  <a class="gallery__link">
+  <a class="gallery__link" href="large-image.jpg">
   <img class="gallery__image"
   src=${photo.preview} 
   data-source=${photo.original} 
@@ -19,32 +19,47 @@ const photos = galleryItems.map((photo) =>
  
 galleryList.insertAdjacentHTML("afterbegin", photos)
 
-galleryList.addEventListener('click', (event) => {
+galleryList.addEventListener('click', onPreviewImage)
+
+
+function onPreviewImage (event) {
+  
+  event.preventDefault()
 
   if (event.target.nodeName !== "IMG") {
     return
   }
 
-  photos = galleryItems.map((photo) => {
+  const originalPhotos = galleryItems.map((photo) => {
+    
     if (event.target.dataset.source !== photo.original) {
     return
-  }
-  const instance = basicLightbox.create(`
-    <img src='${photo.original}' width="800" height="600">
-`)
+    }
+    
+    const instance = basicLightbox.create(
+    `<img src='${photo.original}' width="800" height="600">`)
     instance.show()
 
-    
-  document.addEventListener('keydown', event => {
-    if (event.code === 'Escape') {
-      instance.close()
-    }
-  })
+
 
   })
+}
 
+document.addEventListener('keydown', onEscapeKey)
 
-})
+function onEscapeKey(event) {
+  const moduleImg = document.querySelector('.basicLightbox')
+  if (event.code === 'Escape' && moduleImg) {
+    moduleImg.remove(galleryList)
+    console.log('KAWABANGA');
+  }
+}
 
+// document.addEventListener('keydown', onEscapeKey) 
 
-  
+// function onEscapeKey(event) {
+//   const moduleImg = document.querySelector('.basicLightbox')
+//   if (event.code === 'Escape' && moduleImg) {
+//  document.addEventListener('click', handleClick)
+//   }
+// }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery')
+
+const photos = galleryItems.map((photo) => 
+  `
+  <li class="gallery__item">
+   <a class="gallery__link" href=${photo.original}>
+      <img class="gallery__image" src=${photo.preview} alt="${photo.description}" />
+   </a>
+</li>
+  `
+).join("")
+ 
+galleryList.insertAdjacentHTML("afterbegin", photos)
+
+
+galleryList.addEventListener('click', onPreviewImage)
+
+
+function onPreviewImage(event) {
+  
+  event.preventDefault()
+
+}
